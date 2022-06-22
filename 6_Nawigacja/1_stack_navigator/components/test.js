17056ae488a3f094d89015773357ec59abfaedc1
grzegorzkowalski <grzegorzkowalski@yahoo.com>
2022-06-22 09:35:28 +0200

    Tasks

---
+++ 6_Nawigacja/1_stack_navigator/components/test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const Test = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+};
+
+export default Test;
