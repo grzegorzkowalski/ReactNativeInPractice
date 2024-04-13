896cd373302b52101d069efdcc9566c231915cc2
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-04-13 08:14:02 +0200

    add navigation

--- Z609/hello-world/App.js
+++ Z609/hello-world/App.js
@@ -1,50 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Name from "./components/Name";
-import ColorBoxes from "./components/ColorBoxes";
-import ShowDate from "./components/ShowDate";
-import Map from "./components/Map";
-import DataTimer from "./components/DataTimer";
-import RandomNumberGenerator from "./components/RandomNumberGenerator";
-import {useState} from "react";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import HomePage from "./pages/HomePage";
 
 export default function App() {
-const [randomNumber, setRandomNumber] = useState(0);
-
- console.log("Witajcie studenci");
-
- const rand = () => {
-     const randomNum = Math.random();
-     if (randomNum >= 0.5) {
-         console.log("Liczba większa lub równa 0.5");
-     }
-     else {
-         console.log("Liczba mniejsza niż 0.5");
-     }
-     return randomNum;
- }
-
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Text>{rand()}</Text>
-      <Name />
-      <ColorBoxes />
-      <ShowDate />
-      <Map />
-      <DataTimer />
-      <RandomNumberGenerator updateApp={setRandomNumber}/>
-      <Text>{randomNumber}</Text>
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomePage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Z609/hello-world/pages/HomePage.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import {StatusBar, StyleSheet, Text, View} from "react-native";
+import Name from "../components/Name";
+import ColorBoxes from "../components/ColorBoxes";
+import ShowDate from "../components/ShowDate";
+import Map from "../components/Map";
+import DataTimer from "../components/DataTimer";
+import RandomNumberGenerator from "../components/RandomNumberGenerator";
+
+const HomePage = () => {
+    const [randomNumber, setRandomNumber] = useState(0);
+
+    console.log("Witajcie studenci");
+
+    const rand = () => {
+        const randomNum = Math.random();
+        if (randomNum >= 0.5) {
+            console.log("Liczba większa lub równa 0.5");
+        }
+        else {
+            console.log("Liczba mniejsza niż 0.5");
+        }
+        return randomNum;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+            <Text>{rand()}</Text>
+            <Name />
+            <ColorBoxes />
+            <ShowDate />
+            <Map />
+            <DataTimer />
+            <RandomNumberGenerator updateApp={setRandomNumber}/>
+            <Text>{randomNumber}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+
+
+export default HomePage;
